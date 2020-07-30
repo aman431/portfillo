8026fa3f6db9034f62f431141391a786c1558802
aman rathore <aman@kali.aman>
2020-07-29 23:50:33 -0400

    Update

--- app.js
+++
@@ -1,6 +0,0 @@
-/*const year = document.querySelector('.year');
-console.log(year);
-year.addEventListener('click',function(){
-	var mywindow = window.open("","_self");
-	mywindow.document.write("<img style='width:40px; height:50px;' src='Aman.jpg'>")
-});*/
